1a5d7311b5ed2ba69bd599052767bf4e56f210c4
Sky_Dragonsz <Arthur_Saprunov@hotmail.com>
2018-10-03 19:24:05 +0200

    Added other vanilla flowers to florists bonemeal Added the ability to use vanilla bone meal on sugar canes Added language support for Dutch

--- src/main/java/net/blay09/mods/fertilization/package-info.java
+++ src/main/java/net/blay09/mods/fertilization/package-info.java
@@ -1,6 +1,6 @@
 @MethodsReturnNonnullByDefault
 @ParametersAreNonnullByDefault
-package net.blay09.mods.prettybeaches;
+package net.blay09.mods.fertilization;
 
 import mcp.MethodsReturnNonnullByDefault;
 
