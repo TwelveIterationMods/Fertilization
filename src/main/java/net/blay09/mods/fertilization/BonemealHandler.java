a8366e543357b8a9dc77dbc7765023d089e659aa
blay09 <blay09@hotmail.de>
2018-10-10 21:14:19 +0200

    Merge the bonemeal handler into one method to reduce duplicate code Fix incorrect recipe change from #1

--- src/main/java/net/blay09/mods/fertilization/BonemealHandler.java
+++ src/main/java/net/blay09/mods/fertilization/BonemealHandler.java
@@ -1,10 +1,10 @@
 package net.blay09.mods.fertilization;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
@@ -13,19 +13,24 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class BonemealHandler {
 
     @SubscribeEvent
-    public void onBonemealVines(PlayerInteractEvent.RightClickBlock event) {
-        if (!FertilizationConfig.allowBoneMealOnVines) {
-            return;
-        }
-
+    public void onBonemealVinesAndSugarCanes(PlayerInteractEvent.RightClickBlock event) {
         if (event.getItemStack().getItem() != Items.DYE || event.getItemStack().getMetadata() != 15) {
             return;
         }
 
         IBlockState state = event.getWorld().getBlockState(event.getPos());
-        if (state.getBlock() != Blocks.VINE) {
+        Block growthBlock = state.getBlock();
+        if (growthBlock != Blocks.VINE && growthBlock != Blocks.REEDS) {
             return;
         }
+
+        if (growthBlock == Blocks.REEDS && !FertilizationConfig.allowBoneMealOnSugarCanes) {
+            return;
+        } else if (growthBlock == Blocks.VINE && !FertilizationConfig.allowBoneMealOnVines) {
+            return;
+        }
+
+        boolean growUpwards = growthBlock == Blocks.REEDS;
 
         event.setCancellationResult(EnumActionResult.SUCCESS);
         event.setCanceled(true);
@@ -33,52 +38,11 @@ public class BonemealHandler {
         World world = event.getWorld();
 
         BlockPos candidatePos = event.getPos();
-        while (world.getBlockState(candidatePos).getBlock() == Blocks.VINE) {
-            candidatePos = candidatePos.down();
+        while (world.getBlockState(candidatePos).getBlock() == growthBlock) {
+            candidatePos = growUpwards ? candidatePos.up() : candidatePos.down();
         }
 
-        if (!world.isAirBlock(candidatePos)) {
-            return;
-        }
-
-        if (!world.isRemote) {
-            world.setBlockState(candidatePos, state, 3);
-
-            world.playEvent(2005, event.getPos(), 0);
-        }
-
-        if (!event.getEntityPlayer().capabilities.isCreativeMode) {
-            event.getItemStack().shrink(1);
-        }
-    }
-
-
-    @SubscribeEvent
-    public void onBonemealReeds(PlayerInteractEvent.RightClickBlock event) {
-        if (!FertilizationConfig.allowBoneMealOnSugarCanes) {
-            return;
-        }
-
-        if (event.getItemStack().getItem() != Items.DYE || event.getItemStack().getMetadata() != 15) {
-            return;
-        }
-
-        IBlockState state = event.getWorld().getBlockState(event.getPos());
-        if (state.getBlock() != Blocks.REEDS) {
-            return;
-        }
-
-        event.setCancellationResult(EnumActionResult.SUCCESS);
-        event.setCanceled(true);
-
-        World world = event.getWorld();
-
-        BlockPos candidatePos = event.getPos();
-        while (world.getBlockState(candidatePos).getBlock() == Blocks.REEDS) {
-            candidatePos = candidatePos.up();
-        }
-
-        if (!world.isAirBlock(candidatePos)) {
+        if (!world.isAirBlock(candidatePos) || world.isOutsideBuildHeight(candidatePos)) {
             return;
         }
 
