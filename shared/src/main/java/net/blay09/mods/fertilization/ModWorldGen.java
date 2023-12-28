5b8a60ab117f1c5e3a5027d319154fecbbf6a41b
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2023-12-28 16:32:36 +0100

    feat: Update to Minecraft 1.20.4

--- shared/src/main/java/net/blay09/mods/fertilization/ModWorldGen.java
+++ shared/src/main/java/net/blay09/mods/fertilization/ModWorldGen.java
@@ -4,26 +4,18 @@ import net.blay09.mods.balm.api.world.BalmWorldGen;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.grower.AbstractTreeGrower;
+import net.minecraft.world.level.block.grower.TreeGrower;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
-import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
-import net.minecraft.world.level.levelgen.feature.foliageplacers.FancyFoliagePlacer;
-import net.minecraft.world.level.levelgen.feature.stateproviders.SimpleStateProvider;
-import net.minecraft.world.level.levelgen.feature.trunkplacers.FancyTrunkPlacer;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.OptionalInt;
+import java.util.Optional;
 
 public class ModWorldGen {
-    private static final Map<Block, ResourceKey<ConfiguredFeature<?, ?>>> fancyTrees = new HashMap<>();
-    private static final Map<Block, ResourceKey<ConfiguredFeature<?, ?>>> fancyBeeTrees = new HashMap<>();
+    private static final Map<Block, TreeGrower> treeGrowers = new HashMap<>();
 
     public static void initialize(BalmWorldGen worldGen) {
         registerFancyTree(Blocks.OAK_SAPLING, "oak");
@@ -35,30 +27,14 @@ public class ModWorldGen {
     }
 
     private static void registerFancyTree(Block sapling, String name) {
-        fancyTrees.put(sapling, ResourceKey.create(Registries.CONFIGURED_FEATURE, id("fancy_" + name)));
-        fancyBeeTrees.put(sapling, ResourceKey.create(Registries.CONFIGURED_FEATURE, id("fancy_" + name + "_bees")));
+        final var fancyTree = ResourceKey.create(Registries.CONFIGURED_FEATURE, id("fancy_" + name));
+        final var fancyTreeBees = ResourceKey.create(Registries.CONFIGURED_FEATURE, id("fancy_" + name + "_bees"));
+        treeGrowers.put(sapling, new TreeGrower(name, Optional.empty(), Optional.of(fancyTree), Optional.of(fancyTreeBees)));
     }
 
     @Nullable
-    public static AbstractTreeGrower getFancyTreeForSapling(BlockState state) {
-        ResourceKey<ConfiguredFeature<?, ?>> fancyTree = fancyTrees.get(state.getBlock());
-        ResourceKey<ConfiguredFeature<?, ?>> fancyBeeTree = fancyBeeTrees.get(state.getBlock());
-        if (fancyTree != null && fancyBeeTree != null) {
-            return new FancyTree(fancyTree, fancyBeeTree);
-        }
-
-        return null;
-    }
-
-    private static TreeConfiguration.TreeConfigurationBuilder createFancyTreeConfigBuilder(Block logBlock, Block leavesBlock) {
-        final FancyFoliagePlacer foliagePlacer = new FancyFoliagePlacer(ConstantInt.of(2), ConstantInt.of(4), 4);
-        return (new TreeConfiguration.TreeConfigurationBuilder(
-                SimpleStateProvider.simple(logBlock.defaultBlockState()),
-                new FancyTrunkPlacer(3, 11, 0),
-                SimpleStateProvider.simple(leavesBlock.defaultBlockState()),
-                foliagePlacer,
-                new TwoLayersFeatureSize(0, 0, 0, OptionalInt.of(4))))
-                .ignoreVines();
+    public static TreeGrower getFancyTreeForSapling(BlockState state) {
+        return treeGrowers.get(state.getBlock());
     }
 
     private static ResourceLocation id(String name) {
--- shared/src/main/java/net/blay09/mods/fertilization/item/ExtremelyCompressedBoneMealItem.java
+++ shared/src/main/java/net/blay09/mods/fertilization/item/ExtremelyCompressedBoneMealItem.java
@@ -1,21 +1,15 @@
 package net.blay09.mods.fertilization.item;
 
-import net.blay09.mods.balm.api.Balm;
-import net.blay09.mods.fertilization.BoneMealHelper;
 import net.blay09.mods.fertilization.FertilizationConfig;
 import net.blay09.mods.fertilization.ModWorldGen;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Holder;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.grower.AbstractTreeGrower;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 
 public class ExtremelyCompressedBoneMealItem extends CompressedBoneMealItem {
 
@@ -34,7 +28,7 @@ public class ExtremelyCompressedBoneMealItem extends CompressedBoneMealItem {
         }
 
         BlockState state = level.getBlockState(pos);
-        final AbstractTreeGrower tree = ModWorldGen.getFancyTreeForSapling(state);
+        final var tree = ModWorldGen.getFancyTreeForSapling(state);
         if (FertilizationConfig.getActive().allowBoneMealOnSaplings && tree != null) {
             if (!level.isClientSide) {
                 if (!tree.growTree(((ServerLevel) level), ((ServerLevel) level).getChunkSource().getGenerator(), pos, state, level.random)) {
