b2c91f7659353f5667be011fb0b1b8b24c41e0f7
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-05-23 17:28:02 +0200

    fix: Fix NeoForge compile

--- neoforge/src/main/java/net/blay09/mods/fertilization/NeoForgeFertilization.java
+++ neoforge/src/main/java/net/blay09/mods/fertilization/NeoForgeFertilization.java
@@ -1,12 +1,15 @@
 package net.blay09.mods.fertilization;
 
 import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.balm.neoforge.NeoForgeLoadContext;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 
 @Mod(Fertilization.MOD_ID)
 public class NeoForgeFertilization {
-    public NeoForgeFertilization() {
-        Balm.initialize(Fertilization.MOD_ID, Fertilization::initialize);
+    public NeoForgeFertilization(IEventBus modEventBus) {
+        final var context = new NeoForgeLoadContext(modEventBus);
+        Balm.initialize(Fertilization.MOD_ID, context, Fertilization::initialize);
     }
 
 }
