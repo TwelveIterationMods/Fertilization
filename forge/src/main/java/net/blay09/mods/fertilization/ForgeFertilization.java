d75c8be7a9656cc9aaa33f8aaad6b6d5c1c15a99
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-05-23 15:24:04 +0200

    fix(1.20.6): Fix forge builds

--- forge/src/main/java/net/blay09/mods/fertilization/ForgeFertilization.java
+++ forge/src/main/java/net/blay09/mods/fertilization/ForgeFertilization.java
@@ -1,12 +1,13 @@
 package net.blay09.mods.fertilization;
 
 import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.balm.api.EmptyLoadContext;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(Fertilization.MOD_ID)
 public class ForgeFertilization {
     public ForgeFertilization() {
-        Balm.initialize(Fertilization.MOD_ID, Fertilization::initialize);
+        Balm.initialize(Fertilization.MOD_ID, EmptyLoadContext.INSTANCE, Fertilization::initialize);
     }
 
 }
