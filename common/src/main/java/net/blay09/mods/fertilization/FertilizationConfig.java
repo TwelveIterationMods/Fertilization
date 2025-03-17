512e02293d9609cd463fcc5e0093ba966dabf528
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-03-17 20:52:53 +0100

    feat: Update to Minecraft 1.21.5

--- common/src/main/java/net/blay09/mods/fertilization/FertilizationConfig.java
+++ common/src/main/java/net/blay09/mods/fertilization/FertilizationConfig.java
@@ -1,7 +1,76 @@
 package net.blay09.mods.fertilization;
 
 import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.balm.api.config.reflection.Comment;
+import net.blay09.mods.balm.api.config.reflection.Config;
+import net.blay09.mods.balm.api.config.reflection.NestedType;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
 
+import java.util.Set;
+
+@Config(Fertilization.MOD_ID)
 public class FertilizationConfig {
+
+    @Comment("Set to true if compressed bone meal drops should go straight into the player's inventory.")
+    public boolean addDropsDirectlyToInventory = false;
+
+    @Comment("Set to true if compressed bone meal drops should go straight into the machine's inventory when used automatically.")
+    public boolean addDropsDirectlyToInventoryForFakePlayers = true;
+
+    @Comment("This enables use of extremely compressed bone meal to turn saplings into large fancy trees.")
+    public boolean allowBoneMealOnSaplings = true;
+
+    @Comment("This enables use of normal Bone Meal on vines in order to grow them downwards.")
+    public boolean allowBoneMealOnVines = true;
+
+    @Comment("This enables use of normal Bone Meal on sugar canes in order to grow them upwards.")
+    public boolean allowBoneMealOnSugarCanes = true;
+
+    @Comment("The amount of bone meal applied to the plant when using compressed bone meal.")
+    public int compressedBoneMealPower = 4;
+
+    @Comment("The amount of bone meal applied to the plant when using extremely compressed bone meal.")
+    public int extremelyCompressedBoneMealPower = 36;
+
+    @Comment("The maximum amount of flowers that can spawn when using Florist's Bone Meal on grass.")
+    public int floristsBoneMealMaxFlowers = 5;
+
+    @Comment("The maximum range that flowers can spawn when using Florist's Bone Meal on grass.")
+    public int floristsBoneMealMaxRange = 3;
+
+    @Comment("List of blocks that can be duplicated by using Florist's Bone Meal on them.")
+    @NestedType(ResourceLocation.class)
+    public Set<ResourceLocation> flowerBlocks = Set.of(
+            ResourceLocation.withDefaultNamespace("poppy"),
+            ResourceLocation.withDefaultNamespace("dandelion"),
+            ResourceLocation.withDefaultNamespace("blue_orchid"),
+            ResourceLocation.withDefaultNamespace("allium"),
+            ResourceLocation.withDefaultNamespace("azure_bluet"),
+            ResourceLocation.withDefaultNamespace("red_tulip"),
+            ResourceLocation.withDefaultNamespace("orange_tulip"),
+            ResourceLocation.withDefaultNamespace("white_tulip"),
+            ResourceLocation.withDefaultNamespace("pink_tulip"),
+            ResourceLocation.withDefaultNamespace("oxeye_daisy"),
+            ResourceLocation.withDefaultNamespace("rose_bush"),
+            ResourceLocation.withDefaultNamespace("peony"),
+            ResourceLocation.withDefaultNamespace("lilac"),
+            ResourceLocation.withDefaultNamespace("sunflower"),
+            ResourceLocation.withDefaultNamespace("cornflower"),
+            ResourceLocation.withDefaultNamespace("lily_of_the_valley"));
+
+    public boolean isFlowerBlock(Block block) {
+        final var id = BuiltInRegistries.BLOCK.getKey(block);
+        return flowerBlocks.contains(id);
+    }
+
+    public static FertilizationConfig getActive() {
+        return Balm.getConfig().getActiveConfig(FertilizationConfig.class);
+    }
+
+    public static void initialize() {
+        Balm.getConfig().registerConfig(FertilizationConfig.class);
+    }
 
 }
